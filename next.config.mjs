3b3b2254530cbf61a2908578414fbfa8bbccce59
Dan Politica <73758508+benochi@users.noreply.github.com>
2024-04-19 07:10:43 -0500

    update tailwind config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Enable TypeScript support
+  typescript: {
+    enabled: true,
+  },
+  // Enable PostCSS support
+  postcss: {
+    plugins: [require('tailwindcss')],
+  },
+};
 
 export default nextConfig;
